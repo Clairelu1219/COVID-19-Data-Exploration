c7ee22127d7a49b2110be1ccd1982e2c6d2aac8f
Claire Bilodeau <120172072+Clairelu1219@users.noreply.github.com>
2023-03-02 12:56:55 -1000

    Update Covid19_Data Exploration_2020-2022.sql

--- Covid19_Data Exploration_2020-2022.sql
+++ Covid19_Data Exploration_2020-2022.sql
@@ -1,32 +1,45 @@
---checking data import
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+-- Checking data in these two tables
 SELECT  *
 FROM Covid19.CovidDeaths
+WHERE continent IS NOT NULL
 ORDER BY location, date
-LIMIT 10
+LIMIT 50
 
 SELECT  *
 FROM Covid19.CovidVaccinations
+WHERE continent IS NOT NULL
 ORDER BY location, date
-LIMIT 10
+LIMIT 50
+
 
 -- Select the data we are going to use
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM Covid19.CovidDeaths
+WHERE continent IS NOT NULL
 ORDER BY location, date
 
--- Looking at Totla Cases vs Total Deaths
--- Shows likehood of dying if you contract covid in your country
+
+-- Totla Cases vs Total Deaths
+-- Use wildcard to check likehood of dying if you contract covid in your country
 SELECT 
   location, 
   date, 
   total_cases, 
   total_deaths,
-  ROUND((total_deaths/total_cases) * 100,2) AS death_pect
+  ROUND((total_deaths/total_cases) * 100,2) AS death_percentage
 FROM Covid19.CovidDeaths
 WHERE location LIKE '%States%'
+  AND continent IS NOT NULL
 ORDER BY location, date
 
---Looking at Total Cases vs Population
+
+-- Cases vs Population
+-- Shows what percentage of population infected with Covid
 SELECT 
   location, 
   date,  
@@ -34,27 +47,32 @@ SELECT
   total_cases,
   (total_cases/population) * 100 AS infection_rate
 FROM Covid19.CovidDeaths
-WHERE location LIKE '%States%'
+-- WHERE location LIKE '%States%'
+--   AND continent IS NOT NULL
 ORDER BY location, date
 
--- Looking at Countries with Highest Infection Rate compared to Population
+
+-- Countries with Highest Infection Rate compared to Population
 SELECT 
   location, 
   population,
   MAX(total_cases) AS max_infection_count,
   MAX((total_cases/population) * 100) AS max_infection_rate
 FROM Covid19.CovidDeaths
+WHERE continent IS NOT NULL
 GROUP BY location, population
 ORDER BY max_infection_rate DESC
+
 
 -- Showing Countries with Highest Death Count per Pupulation
 SELECT 
   location,
-  MAX(total_deaths) AS max_total_deaths
+  MAX(total_deaths) AS max_deaths_count
 FROM Covid19.CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
-ORDER BY max_total_deaths DESC
+ORDER BY max_deaths_count DESC
+
 
 -- Showing Continents with Highest Death Count per Pupulation
 SELECT 
@@ -77,6 +95,7 @@ WHERE new_cases > 0 AND continent IS NOT NULL
 GROUP BY date
 ORDER BY date
 
+
 -- Global Numbers: 2020 - 2022 total cases, total deaths and death rate
 SELECT
   SUM(new_cases) AS totl_cases,
@@ -84,6 +103,7 @@ SELECT
   SUM(new_deaths)/SUM(new_cases)*100 AS death_rate
 FROM Covid19.CovidDeaths
 WHERE new_cases > 0 AND continent IS NOT NULL
+
 
 -- Total Population vs Vaccinations
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
@@ -102,6 +122,7 @@ JOIN Covid19.CovidVaccinations AS vac
   AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
 ORDER BY dea.location, dea.date
+
 
 -- Use CTE to perform Calculation on Partition By in previous query
 WITH PopulationvsVaccination AS (
@@ -125,6 +146,7 @@ WITH PopulationvsVaccination AS (
 SELECT *, (rolling_people_vaccinated/population)*100 AS rolling_vaccination_rate
 FROM PopulationvsVaccination
 
+
 -- Using Temp Table to perform Calculation on Partition By in previous query
 CREATE OR REPLACE TEMP TABLE PercentPopulationvsVaccinated AS
 SELECT
@@ -145,9 +167,12 @@ SELECT
 
 SELECT *, (rolling_people_vaccinated/Population)*100
 FROM PercentPopulationvsVaccinated
-LIMIT 10
+
+
+
 
 -- Creating View to store data for later visualizations
+
 CREATE VIEW Covid19.PercentPopulationVaccinatedView AS
 SELECT
     dea.continent,
