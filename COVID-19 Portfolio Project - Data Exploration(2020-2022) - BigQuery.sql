112d21da41caf2a5d6db88f75e7e3d75082212fc
Claire Bilodeau <120172072+Clairelu1219@users.noreply.github.com>
2023-03-06 20:27:27 -1000

    Update COVID-19 Portfolio Project - Data Exploration(2020-2022) - BigQuery.sql

--- COVID-19 Portfolio Project - Data Exploration(2020-2022) - BigQuery.sql
+++ COVID-19 Portfolio Project - Data Exploration(2020-2022) - BigQuery.sql
@@ -188,7 +188,49 @@ SELECT
   ORDER BY dea.location, dea.date
 
 -- Check the View
-SELECT *
-FROM Covid19.PercentPopulationVaccinatedView
+-- SELECT *
+-- FROM Covid19.PercentPopulationVaccinatedView
 
+-- Queries for visualizations
+-- 1.
+SELECT 
+  SUM(new_cases) AS total_cases,
+  SUM(CAST(new_deaths AS int)) AS total_deaths,
+  SUM(CAST(new_deaths AS int))/SUM(New_Cases)*100 AS DeathPercentage
+FROM Covid19.CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2
+
+
+-- 2.
+SELECT location, SUM(CAST(new_deaths AS int)) AS TotalDeathCount
+FROM Covid19.CovidDeaths
+WHERE continent IS NULL 
+AND location NOT IN ('World', 'European Union', 'International')
+GROUP BY location
+ORDER BY TotalDeathCount DESC
+
+
+-- 3.
+SELECT
+ location,
+ population,
+ MAX(total_cases) AS HighestInfectionCount, 
+ MAx((total_cases/population))*100 AS PercentPopulationInfected
+FROM Covid19.CovidDeaths
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected DESC
+
+
+-- 4.
+SELECT 
+  location,
+  population,
+  date,
+  MAX(total_cases) AS HighestInfectionCount, 
+  MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM Covid19.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY Location, Population, date
+ORDER BY PercentPopulationInfected DESC
 
